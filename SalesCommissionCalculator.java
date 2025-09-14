89080fae0cb063dfaca3c3f350d47f333b4aa4fd
Abdulwahab Adamson <adamsonoladipupo@gmail.com>
2025-09-14 22:28:09 +0100

    app update

--- SalesCommissionCalculator.java
+++ SalesCommissionCalculator.java
@@ -16,11 +16,24 @@ public static void main(String[] args){
 System.out.println("Welcome! This app helps calculate your sales");
 Scanner input = new Scanner(System.in);
 
+double itemValue = 0;
+double totalItemValue = 0;
+int count = 0;
 
+
+if (itemValue == 1){
+count = 6;
+}
+while (count < 5){
 System.out.print("Enter items value :");
-double itemValue = input.nextDouble();
+itemValue = input.nextDouble();
+count ++;
+totalItemValue += itemValue;
+}
 
-double commission9Pec = itemValue * 0.09;
+
+
+double commission9Pec = totalItemValue * 0.09;
 double earnings = 200 + commission9Pec;
 
 System.out.printf("Your weekly commission is $%.2f and a total earning is $%.2f",commission9Pec, earnings);
